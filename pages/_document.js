84e9a8d2e6943047a4de564a7b70719a16a2d174
Mike <7515813+mikegazdag@users.noreply.github.com>
2022-08-12 01:28:19 -0700

    Add _document file

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production' && (
+            <meta name="robots" content="noindex" />
+          )}
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Mulish:wght@400..850&display=swap"
+            rel="stylesheet"
+          />
+
+          {/*
+          <meta name="description" content="TBD..." />
+          */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
