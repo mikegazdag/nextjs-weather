c09f2cb5bf2777671b1bb5c103ab4498fb666fcd
Mike <7515813+mikegazdag@users.noreply.github.com>
2022-08-12 01:28:10 -0700

    Add Prettier

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  importOrder: ["^@/(.*)$", "^[./]"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
